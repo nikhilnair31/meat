e7f9b5b141ac45e61eb94f46a334ce5c96e03e8a
Nikhil Nair <niknair31898@gmail.com>
2024-07-19 21:46:54 -0400

    Added pickup for some time

--- Assets/Axtras/Scripts/Player/PlayerInteract.cs
+++ Assets/Axtras/Scripts/Player/PlayerInteract.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class PlayerInteract : MonoBehaviour 
 {
+    private Interactable lastSeenInteractable;
     private Interactable currentHeldItem;
 
     [Header("Main")]
@@ -11,6 +12,8 @@ public class PlayerInteract : MonoBehaviour
     public Transform playerInteractHolder;
     [SerializeField] private LayerMask interactableLayer;
     [SerializeField] private float interactableRange = 5f;
+    [SerializeField] private float interactablePickupInTime = 1f;
+    private float interactablePickupTimer = 0f;
 
     [Header("UI Related")]
     public Image baseCursorImage;
@@ -19,12 +22,25 @@ public class PlayerInteract : MonoBehaviour
     void Update() {
         HandleItemViewing();
         HandleItemInteraction();
+        HandleItemPickupTimer();
+    }
+
+    private void HandleItemPickupTimer() {
+        if (lastSeenInteractable != null) {
+            interactablePickupTimer += Time.deltaTime;
+            if (interactablePickupTimer >= interactablePickupInTime) {
+                lastSeenInteractable = null;
+                interactablePickupInTime = 0f;
+            }
+        }
     }
 
     private void HandleItemViewing() {
         if (Physics.Raycast(playerEyes.position, playerEyes.forward, out RaycastHit hit, interactableRange, interactableLayer)) {
             if (hit.collider.TryGetComponent<Interactable>(out Interactable interactable)) {
-                interactable.ShowUI = true;
+                lastSeenInteractable = interactable;
+                lastSeenInteractable.ShowUI = true;
+                interactablePickupTimer = 0f;
             }
         }
     }
@@ -35,14 +51,19 @@ public class PlayerInteract : MonoBehaviour
         }
     }
     private void ItemInteractRaycast() {
-        if (Physics.Raycast(playerEyes.position, playerEyes.forward, out RaycastHit hit, interactableRange, interactableLayer)) {
-            if (hit.collider.TryGetComponent<Interactable>(out Interactable interactable)) {
-                Debug.Log($"Interact with {hit.collider.name}");
-                if (currentHeldItem != null) {
-                    currentHeldItem.Drop();
+        if (lastSeenInteractable != null) {
+            lastSeenInteractable.Interact();
+        }
+        else {
+            if (Physics.Raycast(playerEyes.position, playerEyes.forward, out RaycastHit hit, interactableRange, interactableLayer)) {
+                if (hit.collider.TryGetComponent<Interactable>(out Interactable interactable)) {
+                    Debug.Log($"Interact with {hit.collider.name}");
+                    if (currentHeldItem != null) {
+                        currentHeldItem.Drop();
+                    }
+                    currentHeldItem = interactable;
+                    interactable.Interact();
                 }
-                currentHeldItem = interactable;
-                interactable.Interact();
             }
         }
     }
